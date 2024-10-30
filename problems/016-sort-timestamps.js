4ca652a19f209c5fdffd30376f3e190735f6790e
Evgeniy.Chukreev <dechukor@gmail.com>
2024-10-30 21:28:46 +0500

    016-sort-timestamp completed

--- problems/016-sort-timestamps.js
+++ problems/016-sort-timestamps.js
@@ -8,8 +8,39 @@
  * @param {string[]} list массив временных моментов представленных в виде строк в формате 'HH:MM:SS'
  * @returns {string[]} отсортированный по возрастанию массив временных моментов
  */
-function sortTimestamps(list) {
-    return undefined;
+function getTimeToSecond (stringTime) {
+    let arrayTime = stringTime.split(':');
+
+    let hours = Number(arrayTime[0]);
+    let minutes = Number(arrayTime[1]);
+    let seconds = Number(arrayTime[2])
+
+    let secondsTime = hours * 3600 + minutes * 60 + seconds;
+
+    return secondsTime;
+} 
+
+function sortTimestamps(list) {    
+    
+    for (let i = 0; i < list.length; i++) {        
+
+        list[i] = {
+            timeStr: list[i],
+            timeSec: getTimeToSecond(list[i])
+        }       
+    }
+
+    list.sort(function(a, b) {
+        
+        return a.timeSec - b.timeSec;
+    })
+
+    for (let i = 0; i < list.length; i++) {  
+
+        list[i] = list[i].timeStr;             
+    }
+
+    return list;
 }
 
 module.exports = sortTimestamps;
