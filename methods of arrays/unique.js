1150f5d1d8b82a1014bb8cd43175b282e46feb7c
Evgeniy.Chukreev <dechukor@gmail.com>
2024-10-31 21:06:51 +0500

    methods of arrays_unique completed

--- methods of arrays/unique.js
+++ methods of arrays/unique.js
@@ -3,6 +3,20 @@
  * @param {string[]} arr - входной массив строк
  * @return {string[]} - массив строк, содержащий только уникальные значения
  */
-function uniqueStrings(arr) {
-    // ваш код здесь
+function uniqueStrings(arr) {    
+    
+    let arrResult = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        
+        if (arr.indexOf(arr[i]) === arr.lastIndexOf(arr[i])) {
+            arrResult.push(arr[i]);
+        }
+
+    }
+
+    return arrResult;
+
 }
+
+console.log(uniqueStrings(['f', 'wert', '', 'f', 'a', 'a', 'ert', 'A', 'ze', 'f']));
