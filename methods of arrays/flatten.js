3c0b8d99a12be43e28540a5f981d45bfc6f8505f
Evgeniy.Chukreev <dechukor@gmail.com>
2024-10-31 18:15:29 +0500

    methods of arrays_flatten completed

--- methods of arrays/flatten.js
+++ methods of arrays/flatten.js
@@ -5,5 +5,7 @@
     Например, flatten([1, [2, 3], [4, [5, 6]]]) должна вернуть [1, 2, 3, 4, 5, 6].
 * */
 function flatten(arr) {
-    // Your code
+    return arr.flat(Infinity);
 }
+
+console.log(flatten([1, [2, 3], [4, [5, 6]]]));
