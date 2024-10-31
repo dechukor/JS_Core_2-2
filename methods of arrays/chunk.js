897f87ccd0b537f1dad65bdffb1adcdcf8658361
Evgeniy.Chukreev <dechukor@gmail.com>
2024-10-31 18:12:32 +0500

    methods of arrays_chunk completed

--- methods of arrays/chunk.js
+++ methods of arrays/chunk.js
@@ -3,5 +3,15 @@
  состоящий из подмассивов длиной не более size. Например, chunk([1, 2, 3, 4, 5], 2) должна вернуть [[1, 2], [3, 4], [5]].
  * */
 function chunk(arr, size) {
-    // Your code
+    
+    arrResult = [];
+    for (let i = 0; i < arr.length; i += size) {
+
+        arrResult.push(arr.slice(i, i + size));
+
+    }
+    
+    return arrResult;    
 }
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
