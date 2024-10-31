e4c4a3e040b002c4e053c6eb753815e834ad117c
Evgeniy.Chukreev <dechukor@gmail.com>
2024-10-31 20:34:49 +0500

    methods of arrays_shuffle completed

--- methods of arrays/shuffle.js
+++ methods of arrays/shuffle.js
@@ -3,6 +3,23 @@
  порядке. Функция должна изменять исходный массив, а не возвращать новый. Например, shuffle([1, 2, 3, 4, 5]) может
  вернуть [3, 1, 5, 4, 2].
  * */
-function shuffle(arr1, arr2) {
-    // Your code
+function shuffle(arr) {
+    
+    quantityTransposition = 10;
+    let i = 0;
+
+    while (i < quantityTransposition) {
+    
+       let randomIndex1 =  Math.floor(Math.random() * arr.length);
+       let randomIndex2 =  Math.floor(Math.random() * arr.length);
+
+       let temp = arr[randomIndex1];
+       arr[randomIndex1] = arr[randomIndex2];
+       arr[randomIndex2] = temp;
+
+       i++;
+    }
+    return arr;
 }
+
+console.log(shuffle([1, 2, 3, 4, 5]));
